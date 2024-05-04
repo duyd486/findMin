547ecb50a6f70a754aafc98643b8c593688437b2
duyd486 <duyd486@gmail.com>
2024-05-04 16:22:55 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+
+    public static int Min(int []arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.Length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+    private static void Main(string[] args)
+    {
+        int []arr = {4, 12, 7, 8, 1, 6, 9};
+        Console.WriteLine("So nho nhat trong day la: " + Min(arr));
+    }
+}
